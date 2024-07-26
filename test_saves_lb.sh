9787534606c1b9422c1a64b426a4be3e08d81c02
killerbee <killerbee13.0197@gmail.com>
2024-07-26 16:45:31 -0700

    test_saves_lb.sh now reports incorrect scores
    - New wrong-file option, defaults to fail-file (previous version logged to
    success-file)
    - Scores and expected scores now written to list files

--- test_saves_lb.sh
+++ test_saves_lb.sh
@@ -5,7 +5,7 @@ function exists
 	or return
 end
 
-argparse --name=test_saves.sh 'i/interactive' 'd/save-dir=!exists' 's/success-file=?' 'f/fail-file=?' 'q/quiet' 'r/random=' 'n' 'loglevel=' 'limit=!_validate_int --min 0' -- $argv
+argparse --name=test_saves.sh 'i/interactive' 'd/save-dir=!exists' 's/success-file=?' 'f/fail-file=?' 'w/wrong-file=?' 'q/quiet' 'r/random=' 'n' 'loglevel=' 'limit=!_validate_int --min 0' -- $argv
 or return
 
 set -l save_dir $_flag_d
@@ -17,6 +17,12 @@ if not set -q _flag_f
 	set _flag_f /dev/null
 end
 set -l fail_file $_flag_f
+# wrong-file defaults to fail-file
+if not set -q _flag_w
+	set _flag_w $fail_file
+end
+set -l wrong_file $_flag_w
+
 if set -q _flag_r
 	set _flag_r -r $_flag_r
 end
@@ -24,17 +30,13 @@ if set -q _flag_n
 	set _flag_n --fixed false
 end
 if set -q _flag_loglevel
-	echo true
 	set loglevel --loglevel $_flag_loglevel
 else
-	echo false
 	set loglevel
 end
 if set -q _flag_limit
-	echo true
 	set limit --limit $_flag_limit
 else
-	echo false
 	set limit
 end
 
@@ -49,26 +51,42 @@ set -l saves 00150 10981 20176 21340 22280 30647 31904 32050 33762 40196 41427 4
 
 set -l files_count 0
 set -l success_count 0
+set -l wrong_count 0
 set -l fail_count 0
 
 echo > $success_file
 echo > $fail_file
+echo > $wrong_file
 
+set tmp_result (mktemp)
+
+function filter_result
+	tail -n 1 $tmp_result | sed -Ee 's/^score: //' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+end
 
 for id in $saves
 	for file in $save_dir/$id/$id*
 		set files_count (math $files_count + 1)
+		set -l RE "s@$id\.([0-9]+)-([0-9]+)-([0-9]+)-?(a?c?).txt@\1/\2/\3/\4@"
+		set -l expected (basename $file | sed -Ee $RE -e 's@/$@@')
+		
 		echo ./TIS-100-CXX $_flag_q $_flag_r $_flag_n $loglevel $limit -c (basename $file) $id
-		if ./TIS-100-CXX $_flag_q $_flag_r $_flag_n $loglevel $limit -c $file $id
-			echo $file >> $success_file
-			set success_count (math $success_count + 1)
-		else 
-			echo $file >> $fail_file
+		./TIS-100-CXX $_flag_q $_flag_r $_flag_n $loglevel $limit -c $file $id | tee $tmp_result
+		if test $pipestatus[1] -eq 0
+			set -l result (filter_result)
+			if test $result = $expected
+				echo $file $result $expected >> $success_file
+				set success_count (math $success_count + 1)
+			else
+				echo $file $result $expected >> $wrong_file
+				set wrong_count (math $wrong_count + 1)
+			end
+		else
+			set -l result (filter_result)
+			echo $file $result $expected >> $fail_file
 			set fail_count (math $fail_count + 1)
 		end
-		
-		set -l RE "s@$id\.([0-9]+)-([0-9]+)-([0-9]+)-?(a?c?).txt@\1/\2/\3/\4@"
-		echo (basename $file | sed -Ee $RE | sed -Ee 's@/$@@')
+		echo $expected
 		fgrep '##' $file
 		if set -q _flag_i
 			read _
@@ -77,4 +95,5 @@ for id in $saves
 	end
 end
 
-echo "$files_count saves tested. $success_count validated, $fail_count failed."
+echo "$files_count saves tested. $success_count validated, $wrong_count scored incorrectly, $fail_count failed."
+rm $tmp_result
