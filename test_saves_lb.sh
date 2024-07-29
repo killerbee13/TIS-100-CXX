624d4a9b43e8c85717a4e9b13a36171b8d633116
killerbee <killerbee13.0197@gmail.com>
2024-07-29 15:45:31 -0700

    Added TIS-NET support to test_saves_lb.sh
    - Removed manual list of segment IDs
    
    Total now of 552 saves to test

--- test_saves_lb.sh
+++ test_saves_lb.sh
@@ -28,14 +28,6 @@ if set -q _flag_n
 end
 
 
-# all implemented saves
-set -l saves 00150 10981 20176 21340 22280 30647 31904 32050 33762 40196 41427 42656 43786 50370 51781 52544 53897 60099 61212 62711 63534 70601 UNKNOWN
-#set -l saves 33762 40196 41427 42656 43786 50370 51781 52544 53897 60099 61212 62711 63534 70601 UNKNOWN NEXUS.11.711.2 NEXUS.12.534.4
-# achievements only
-#set -l saves 00150 21340 42656
-# images only
-#set -l saves 50370 51781 53897 70601 #NEXUS.11.711.2 NEXUS.12.534.4 NEXUS.13.370.9 NEXUS.14.781.3
-
 set -l files_count 0
 set -l success_count 0
 set -l wrong_count 0
@@ -51,34 +43,39 @@ function filter_result
 	tail -n 1 $tmp_result | sed -Ee 's/^score: //' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | tr z c
 end
 
-for id in $saves
-	for file in $save_dir/$id/$id*
-		set files_count (math $files_count + 1)
-		set -l RE "s@$id\.([0-9]+)-([0-9]+)-([0-9]+)-?(a?c?).txt@\1/\2/\3/\4@"
-		set -l expected (basename $file | sed -Ee $RE -e 's@/$@@')
-		
-		echo ./TIS-100-CXX $_flag_n $argv -c (basename $file) $id
-		./TIS-100-CXX $_flag_n $argv -c $file $id | tee $tmp_result
-		if test $pipestatus[1] -eq 0
-			set -l result (filter_result)
-			if test $result = $expected
-				echo "$file; $result; $expected" >> $success_file
-				set success_count (math $success_count + 1)
+for map in $save_dir/TIS*
+	echo $map
+	for id_folder in $map/*
+		echo $id_folder
+		set id (basename $id_folder)
+		for file in $id_folder/$id*
+			set files_count (math $files_count + 1)
+			set -l RE "s@$id\.([0-9]+)-([0-9]+)-([0-9]+)-?(a?c?).txt@\1/\2/\3/\4@"
+			set -l expected (basename $file | sed -Ee $RE -e 's@/$@@')
+			
+			echo ./TIS-100-CXX $_flag_n $argv -c (basename $file) $id
+			./TIS-100-CXX $_flag_n $argv -c $file $id | tee $tmp_result
+			if test $pipestatus[1] -eq 0
+				set -l result (filter_result)
+				if test $result = $expected
+					echo "$file; $result; $expected" >> $success_file
+					set success_count (math $success_count + 1)
+				else
+					echo "$file; $result; $expected" >> $wrong_file
+					set wrong_count (math $wrong_count + 1)
+				end
 			else
-				echo "$file; $result; $expected" >> $wrong_file
-				set wrong_count (math $wrong_count + 1)
+				set -l result (filter_result)
+				echo "$file; $result; $expected" >> $fail_file
+				set fail_count (math $fail_count + 1)
 			end
-		else
-			set -l result (filter_result)
-			echo "$file; $result; $expected" >> $fail_file
-			set fail_count (math $fail_count + 1)
+			echo $expected
+			fgrep '##' $file
+			if set -q _flag_i
+				read _
+			end
+			echo
 		end
-		echo $expected
-		fgrep '##' $file
-		if set -q _flag_i
-			read _
-		end
-		echo
 	end
 end
 
