killerbee13/TIS-100-CXX

commit 2067ab46429992da939d553c793db922e8653900

Have numeric output nodes remember their validation status
he checking logic is much more readable and slightly faster as a bonus

main.cpp
@@ -74,17 +74,19 @@ score run(field& l, int cycles_limit, bool print_err) {
 	sc.validated = true;
 
 	log_flush();
-	for (auto it = l.end_regular(); it != l.end(); ++it) {
+	for (auto it = l.begin_output(); it != l.end(); ++it) {
 		auto n = it->get();
 		if (type(n) == node::out) {
 			auto p = static_cast<output_node*>(n);
-			if (p->outputs_expected != p->outputs_received) {
+			if (p->wrong) {
 				sc.validated = false;
+				break;
 			}
 		} else if (type(n) == node::image) {
 			auto p = static_cast<image_output*>(n);
 			if (p->image_expected != p->image_received) {
 				sc.validated = false;
+				break;
 			}
 		}
 	}

io.hpp
@@ -72,7 +72,7 @@ struct output_node : node {
 	type_t type() const noexcept override { return out; }
 	// Attempt to read from neighbor every step
 	void step() override {
-		if (outputs_expected.size() == outputs_received.size()) {
+		if (complete) {
 			return;
 		}
 		if (auto r = do_read(neighbors[port::up], port::down)) {
@@ -80,12 +80,18 @@ struct output_node : node {
 			auto i = outputs_received.size();
 			outputs_received.push_back(*r);
 			if (outputs_received[i] != outputs_expected[i]) {
+				wrong = true;
 				log_debug("incorrect value written");
 			}
+			complete = (outputs_expected.size() == outputs_received.size());
 		}
 	}
 	void finalize() override {}
-	void reset() noexcept override { outputs_received.clear(); }
+	void reset() noexcept override {
+		outputs_received.clear();
+		wrong = false;
+		complete = false;
+	}
 	std::optional<word_t> emit(port) override { return std::nullopt; }
 	std::string print() const override {
 		std::ostringstream ret;
@@ -100,6 +106,8 @@ struct output_node : node {
 	std::string filename;
 	io_type_t io_type;
 	char d{' '};
+	bool wrong{false};
+	bool complete{false};
 };
 struct image_output : node {
 	using node::node;

parser.hpp
@@ -57,40 +57,28 @@ class field {
 	}
 
 	bool active() const {
-		bool r{};
-		bool all_outputs_satisfied{true};
-		bool all_outputs_correct{true};
+		bool active{};
 		for (auto it = begin_output(); it != end(); ++it) {
 			if (type(it->get()) == node::out) {
 				auto i = static_cast<const output_node*>(it->get());
-				if (i->outputs_received.size() < i->outputs_expected.size()) {
-					r = true;
-					all_outputs_satisfied = false;
+				if (not i->complete) {
+					active = true;
 
-					if (i->outputs_received.size() > i->outputs_expected.size()) {
-						all_outputs_correct = false;
-					}
 // speed up simulator by failing early when an incorrect output is written
 #if RELEASE
-					else if (auto k = i->outputs_received.size() - 1;
-					         not i->outputs_received.empty()
-					         and i->outputs_received[k] != i->outputs_expected[k]) {
-						all_outputs_correct = false;
+					if (i->wrong) {
+						return false;
 					}
 #endif
 				}
 			} else if (type(it->get()) == node::image) {
 				auto i = static_cast<const image_output*>(it->get());
 				if (i->image_expected != i->image_received) {
-					r = true;
-					all_outputs_satisfied = false;
+					active = true;
 				}
 			}
 		}
-		if (all_outputs_satisfied or not all_outputs_correct) {
-			return false;
-		}
-		return r;
+		return active;
 	}
 
 	/// Write the full state of all nodes, similar to what the game displays in

commit 10b3620d30741240f6279e1a53ba173b0cdedc49

Empty numeric outputs are actually not valid, who'd have guessed
Fixes: 2067ab4

main.cpp
@@ -78,7 +78,7 @@ score run(field& l, int cycles_limit, bool print_err) {
 		auto n = it->get();
 		if (n->type() == node::out) {
 			auto p = static_cast<output_node*>(n);
-			if (p->wrong) {
+			if (p->wrong || !p->complete) {
 				sc.validated = false;
 				break;
 			}