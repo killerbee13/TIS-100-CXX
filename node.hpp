6eabddbf7befbd0df1aff0033e16faef5eb0e6a7
killerbee <killerbee13.0197@gmail.com>
2024-08-04 14:01:09 -0700

    Fix copyright notices, attribute Andrea
    I keep forgetting that my IDE template is broken

--- node.hpp
+++ node.hpp
@@ -1,6 +1,6 @@
 /* *****************************************************************************
  * TIX-100-CXX
- * Copyright (c) 2024 killerbee
+ * Copyright (c) 2024 killerbee, Andrea Stacchiotti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
