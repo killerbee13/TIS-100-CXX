b1e006c45225d2713a8f687c1458f6220e8f8b73
killerbee <killerbee13.0197@gmail.com>
2024-08-04 17:18:13 -0700

    Fixed pixel normalization (again)
    Check has to happen before casting to enum

--- node.hpp
+++ node.hpp
@@ -213,18 +213,18 @@ struct tis_pixel {
 	    // a shader that darkens them
 	};
 
-	static color normalize(color c) {
+	static color normalize(auto c) {
 		if (c > 3 or c < 0) {
 			return C_black;
 		}
-		return c;
+		return static_cast<color>(c);
 	}
 
 	tis_pixel() = default;
 	constexpr tis_pixel(color c) noexcept
 	    : val(normalize(c)) {}
 	explicit(false) constexpr tis_pixel(std::integral auto c) noexcept
-	    : val(normalize(static_cast<color>(c))) {}
+	    : val(normalize(c)) {}
 
 	pnm::rgb_pixel as_rgb() const { return colors[val]; }
 	constexpr operator pnm::rgb_pixel() const { return colors[val]; }
